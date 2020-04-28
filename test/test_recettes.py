81778feed4c97dbe5022c7814cb2f1899bee745f
Keurfon Luu <keurfonluu@outlook.com>
2020-04-28 09:29:32 -0700

    add unit tests

---
+++ test/test_recettes.py
@@ -0,0 +1,54 @@
+from pingo_api.scraper import create_recipe
+
+import pytest
+
+
+@pytest.mark.parametrize(
+    "url, recipe_ref",
+    [
+        (
+            "https://www.cuisineaz.com/recettes/pot-au-feu-traditionnel-5620.aspx",
+            {
+                "name": "Pot-au-feu traditionnel",
+                "servings": "6",
+                "ingredients": 15,
+                "directions": 4,
+            },
+        ),
+        (
+            "https://cuisine.journaldesfemmes.fr/recette/176020-pot-au-feu",
+            {
+                "name": "Pot-au-feu : la meilleure recette",
+                "servings": "8",
+                "ingredients": 0,  # To fix
+                "directions": 6,
+            },
+        ),
+        (
+            "https://www.marmiton.org/recettes/recette_pot-au-feu_32792.aspx",
+            {
+                "name": "Pot-au-feu",
+                "servings": "4",
+                "ingredients": 13,
+                "directions": 10,
+            },
+        ),
+        (
+            "https://www.750g.com/pot-au-feu-r1452.htm",
+            {
+                "name": "Pot au feu",
+                "servings": "6",
+                "ingredients": 17,
+                "directions": 4,
+            },
+        ),
+    ],
+)
+def test_recettes(url, recipe_ref):
+    recipe = create_recipe(url)
+
+    assert url == recipe.url
+    assert recipe_ref["name"] == recipe.name
+    assert recipe_ref["servings"] == recipe.servings
+    assert recipe_ref["ingredients"] == len(recipe.ingredients)
+    assert recipe_ref["directions"] == len(recipe.directions)
