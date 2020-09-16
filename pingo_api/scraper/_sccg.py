bea9df1ba0d4b8f7ce7c5112dd96b70cb303902c
keurcien <keurcien.luu@gmail.com>
2020-09-16 23:49:34 +0200

    Fixed CuisineAZ scraping

--- pingo_api/scraper/_sccg.py
+++ pingo_api/scraper/_sccg.py
@@ -13,14 +13,13 @@ class Recette750g(RecetteBase):
         super().__init__(url)
 
     def _get_name(self, soup):
-        self._name = soup.find("span", {"class": "u-title-page"}).text
+        self._name = soup.find("span", {"class": "recipe-title"}).text.strip()
 
     def _get_ingredients(self, soup):
         self._ingredients = []
 
-        ingredients = soup.find("ul", {"class": "c-recipe-ingredients__list"}).findAll(
-            "li"
-        )
+        ingredients = soup.findAll("span", {"class": "recipe-ingredients-item-label"})
+
         for ingredient in ingredients:
             if ingredient.span:
                 self._ingredients.append(ingredient.span.previous_sibling.strip())
@@ -28,12 +27,12 @@ class Recette750g(RecetteBase):
                 self._ingredients.append(ingredient.text)
 
     def _get_directions(self, soup):
-        directions = soup.findAll("div", {"class": "c-recipe-steps__item-content"})
+        directions = soup.findAll("div", {"class": "recipe-steps-text"})
         self._directions = [direction.text for direction in directions]
 
     def _get_servings(self, soup):
-        if soup.find("span", {"class": "c-ingredient-variator-label"}):
-            servings = soup.find("span", {"class": "c-ingredient-variator-label"}).text
+        if soup.find("span", {"class": "ingredient-variator-label"}):
+            servings = soup.find("span", {"class": "ingredient-variator-label"}).text
         else:
             servings = soup.find("h2", {"class": "u-title-section"}).text
 
